25cd0a7118050de6c03371cebc0fdda1dfccd947
shimgo <fen.mex.cyc@gmail.com>
2023-12-22 21:31:21 +0900

    git grep でignore caseを有効に

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -440,7 +440,7 @@ vim.opt.rtp:append('/opt/homebrew/opt/fzf')
 vim.keymap.set("n", "<leader>ff", ":FzfLua files<CR>")
 vim.keymap.set("n", "<leader>fl", ":FzfLua buffers<CR>") -- 今開いているバッファをファイル名で検索
 vim.keymap.set("n", "<leader>fq", ":FzfLua quickfix<CR>")
-vim.keymap.set("n", "<leader>fgg", ":lua require'fzf-lua'.grep({ cmd = 'git grep --line-number --column --color=always' })<CR>") -- Git管理のファイルをgrep
+vim.keymap.set("n", "<leader>fgg", ":lua require'fzf-lua'.grep({ cmd = 'git grep -i --line-number --column --color=always' })<CR>") -- Git管理のファイルをgrep
 vim.keymap.set("n", "<leader>fgb", ":FzfLua grep_curbuf<CR>") -- 今開いているバッファをgrep
 vim.keymap.set("n", "<leader>fgi", ":FzfLua grep<CR>") -- すべてのファイルをgrep
 
