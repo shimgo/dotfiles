3b7f651a76ce1baf2b54ed46e41fb203bd87b2d6
shimgo <fen.mex.cyc@gmail.com>
2025-04-07 10:20:12 +0900

    neoscroll: スクロールアニメーションの速度を高速化
    neoscrollプラグインのduration_multiplierの値を0.1から0.01に変更し、
    スクロールアニメーションの速度を10倍速く設定しました。これにより
    スムーズさを保ちながらもより素早いスクロール体験が可能になります。

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -982,7 +982,7 @@ require('neoscroll').setup({
   stop_eof = true,             -- Stop at <EOF> when scrolling downwards
   respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
   cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-  duration_multiplier = 0.1,   -- Global duration multiplier デフォルトから変えた
+  duration_multiplier = 0.01,   -- Global duration multiplier デフォルトから変えた
   easing = 'linear',           -- Default easing function
   pre_hook = nil,              -- Function to run before the scrolling animation starts
   post_hook = nil,             -- Function to run after the scrolling animation ends
